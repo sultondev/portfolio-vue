6b1704c12d5a1f5cab152c3d5311cb7790e667fa
Sultonkhon Oblokulov <sultondeveloper@gmail.com>
2022-11-15 20:00:19 +0500

    bismillah

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { RouterView } from "vue-router"
+</script>
+
+<template>
+  <RouterView></RouterView>
+</template>
+
+<style lang="sass">
+@import "./assets/styles/general.sass"
+
+body
+  background-color: var(--clr-secondary)
+</style>
