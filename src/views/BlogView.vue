ed4827ceb0700038a83d72fed22dbb7b6868e8e3
Sultonkhon <74731394+sultondev@users.noreply.github.com>
2022-11-25 19:10:10 +0500

    Sections/layout (#19)
    * create 3 sections
    
    * fix unresolved image path
    
    * All page layout
    
    * remove npm package manager from the project
    
    * fix script problem
    
    * fix overflow problem with SectInfo component
    
    * fix responsive of website
    
    * fix problem with package manager
    
    * fix problems with UI
    
    * converting svg icons to fonts
    
    * fix ui
    
    * fix router
    
    * fix ui and routing problems
    
    * fetching data for portfolio section
    
    * change baseurl to online back-end server
    
    * fix typescript issues
    
    * fix issues with TypeScript
    
    * fix ui of portfolio
    
    * fix ui of main page
    
    * fix recommend section
    
    * fix exp section
    
    * add favicon
    
    * add design to cards

--- src/views/BlogView.vue
+++ src/views/BlogView.vue
@@ -1,12 +1,17 @@
 <template>
-  <section>
+  <section class="blog">
     <div class="container">
-      <h6>Blogs</h6>
+      <h6 class="general__titles blog__title">💬 Blog</h6>
     </div>
   </section>
 </template>
 
-<style lang="sass">
-h6
-  font-size: var(--heading-30)
+<style lang="sass" scoped>
+.blog
+  padding: 40px 0
+
+  &__title
+    font-size: var(--heading-30)
+    text-align: center
+    margin: 0 auto
 </style>
